02a46d827d4ddedcddf1f1fbd9b93473482abc8c
Gólib Abdullayev <golibabdullaev8@gmail.com>
2025-10-20 06:57:54 +0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["msg"]);
+console.log(props.msg);
+</script>
+
+<template>
+    <div
+        class="w-full font-display font-medium min-h-screen bg-slate-900 flex items-center justify-center text-white"
+    >
+        <div>
+            <Button
+                class="border rounded text-black bg-white px-4 py-1 cursor-pointer"
+                >Sign up</Button
+            >
+            <Button
+                class="border rounded text-black bg-white px-4 py-1 cursor-pointer"
+                >Sign in</Button
+            >
+        </div>
+    </div>
+</template>
